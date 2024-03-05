338f86b9bea728bbaafa6d958c68d5c1ed9b24dd
FhershFS <39203607@uaz.edu.mx>
2024-03-05 11:22:41 -0600

    Agregado fuego al sol y correciones visuales

--- BasicGame/src/mygame/Main.java
+++ BasicGame/src/mygame/Main.java
@@ -1,9 +1,12 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
@@ -35,7 +38,24 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         // Creación de los planetas con sus atributos y colores
-        sun = createPlanet("Sun", ColorRGBA.Yellow, 0f, 0f, 0.5f, 1.7f);
+    sun = createPlanet("Sun", ColorRGBA.Yellow, 0f, 0f, 0.5f, 1.7f);
+            /** Uses Texture from jme3-test-data library! */
+    ParticleEmitter fireEffect = new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 30);
+    Material fireMat = new Material(assetManager, "Common/MatDefs/Misc/Particle.j3md");
+    //fireMat.setTexture("Texture", assetManager.loadTexture("Effects/Explosion/flame.png"));
+    fireEffect.setMaterial(fireMat);
+    fireEffect.setImagesX(2); fireEffect.setImagesY(2); // 2x2 texture animation
+    fireEffect.setEndColor( new ColorRGBA(1f, 0f, 0f, 1f) );   // red
+    fireEffect.setStartColor( new ColorRGBA(1f, 1f, 0f, 0.5f) ); // yellow
+    fireEffect.getParticleInfluencer().setInitialVelocity(new Vector3f(0, 2, 0));
+    fireEffect.setStartSize(0.6f);
+    fireEffect.setEndSize(0.1f);
+    fireEffect.setGravity(0f,0f,0f);
+    fireEffect.setLowLife(0.5f);
+    fireEffect.setHighLife(3f);
+    fireEffect.getParticleInfluencer().setVelocityVariation(0.3f);
+    rootNode.attachChild(fireEffect);
+
         earth = createPlanet("Earth", ColorRGBA.Blue, 5f, 1.0f, 0.5f, 1f);
         moon = createPlanet("Moon", ColorRGBA.Gray, 1.5f, 3.0f, 1.5f, 0.5f); 
         planet1 = createPlanet("Planet1", ColorRGBA.Red, 8f, 1.3f, 0.8f, 1.2f);
@@ -62,15 +82,15 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleUpdate(float tpf) {
         // Rotar todo el sistema solar alrededor del eje Y
-        rootNode.rotate(0, tpf * 0.1f, 0);
+        rootNode.rotate(0, tpf * 0.2f, 0);
 
         // Actualizar posiciones y rotaciones de los planetas
         updatePlanet(earth, tpf, 1.0f, 0.5f);
         updatePlanet(moon, tpf, 3.0f, 1.5f);
-        updatePlanet(planet1, tpf, 0.8f, 2.0f);
+        updatePlanet(planet1, tpf, 0.8f, 1.0f);
         updatePlanet(planet2, tpf, 1.2f, 1.2f);
         updatePlanet(planet3, tpf, 1.5f, 1.8f);
-        updatePlanet(planet4, tpf, 2.0f, 2.5f);
+        updatePlanet(planet4, tpf, 2.0f, 1.5f);
     }
 
     // Método para crear un planeta con su forma, material y atributos iniciales
@@ -95,7 +115,7 @@ public class Main extends SimpleApplication {
         planet.rotate(0, FastMath.nextRandomFloat() * FastMath.PI, 0);
 
         // Establecer la velocidad de órbita como datos de usuario del nodo
-        planet.setUserData("OrbitSpeed", orbitSpeed);
+         planet.setUserData("OrbitSpeed", orbitSpeed / distance);  // Ajusta el factor para los planetas más lejanos
 
         return planet;
     }
